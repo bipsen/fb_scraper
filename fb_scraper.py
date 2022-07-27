9e881dced4485c729e5fc9d059d19388d3050e84
bipsen <ipsen.bertil@gmail.com>
2022-07-27 10:15:02 +0200

    Added script

---
+++ fb_scraper.py
@@ -0,0 +1,127 @@
+from pathlib import Path
+import json
+from datetime import date
+from time import sleep
+import logging
+import facebook_scraper
+from ratelimit import limits, sleep_and_retry
+import click
+
+logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s: %(message)s", level=logging.NOTSET
+)
+
+
+@sleep_and_retry
+@limits(calls=15, period=900)
+def check_limit():
+    """Dummy function to use the ratelimit library with
+    the post generator"""
+    return
+
+
+start_url = None
+
+
+def handle_pagination_url(url):
+    """To paginate scraping between script breaks"""
+    global start_url
+    start_url = url
+
+
+def try_to_resume(resume_file):
+    """Resume from file if exists"""
+    if resume_file.exists():
+        with open(resume_file, "r") as f:
+            start_url = f.read()
+        logging.info(f"Resuming from file {resume_file}")
+        return start_url
+
+
+def remove_resume_file(resume_file):
+    if resume_file.exists():
+        resume_file.unlink()
+
+
+@click.option(
+    "--date-start", type=click.DateTime(formats=["%Y-%m-%d"]), default=str(date.min())
+)
+@click.option(
+    "--date-end", type=click.DateTime(formats=["%Y-%m-%d"]), default=str(date.max())
+)
+def main(date_start, date_end):
+
+    # File with target group ids
+    with open("groups.txt", "r") as f:
+        groups = f.read().splitlines()
+
+    # Keeps track of already done groups
+    with open("finished_groups.txt", "r") as f:
+        finished_groups = f.read().splitlines()
+    groups = [g for g in groups if not g in finished_groups]
+
+    facebook_scraper.set_cookies("cookies.txt")
+
+    k = 0  # For casual time window
+
+    for group in groups:
+
+        logging.info(f"Now harvesting {group}")
+
+        resume_file = Path(f"start_url_{group}")
+        start_url = try_to_resume(resume_file)
+
+        # Create download folder
+        download_dir = Path("downloads") / group
+        download_dir.mkdir(exist_ok=True, parents=True)
+
+        while True:
+            try:
+                for post in facebook_scraper.get_posts(
+                    group,
+                    page_limit=None,
+                    start_url=start_url,
+                    request_url_callback=handle_pagination_url,
+                ):
+
+                    check_limit()
+
+                    # Check time
+                    if post["time"] > date_end:
+                        continue
+                    elif post["time"] <= date_start:
+                        k += 1
+                        if k == 20:
+                            break
+                        else:
+                            continue
+                    else:
+                        # If post within time frame, save post
+                        with open(download_dir / f"{post['post_id']}.json", "w") as f:
+                            json.dump(post, f)
+                        k = 0
+
+                logging.info(f"Completed {group}")
+                remove_resume_file(resume_file)
+                break
+
+            except facebook_scraper.exceptions.TemporarilyBanned:
+                logging.info(
+                    f"Temporary ban while harvesting {group}. Sleeping 10 minutes."
+                )
+                sleep(600)
+
+            except Exception as e:
+                with open(resume_file, "w") as f:
+                    f.write(start_url)
+                logging.error("Saved resume info")
+                raise e
+
+        # Save finished group
+        with open("finished_groups.txt", "a"):
+            f.write(group, "\n")
+
+
+if __name__ == "__main__":
+    main()
